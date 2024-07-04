a884353ad0f2ddaf5cb3bc07139ecbfb21c82f8a
olejean <ffpupkin@yandex.ru>
2024-07-04 22:50:30 +0300

    zadanie_gotovo

---
+++ roles/app/files/app/index.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "mysql";
+$username = "oleg";
+$password = "password";
+$dbname = "mydb";
+
+// Создание соединения
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверка соединения
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Выполнение SQL запроса
+$sql = "SELECT id, name, email FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Вывод данных каждой строки
+    echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"]. "</td><td>" . $row["email"]. "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
